d10599782f9ac95102367c2281568cdfa51f3068
cip-Mbrain <ennocentcip@gmail.com>
2025-05-15 02:22:08 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function sayHello() {
+  console.log('Hello World!!');
+}
+
+function greeting() {
+  console.log("what's good and how are you feeling today! ");
+}
+
+function add(add1, add2) {
+  // console.log(add1 + add2);
+  return add1 + add2;
+}
+
+const result = add(2, 4);
+
+// console.log(result);
+
+function registerUser(user) {
+  return user + ' is registered';
+}
+
+console.log(registerUser('John'));
