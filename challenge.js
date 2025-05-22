50df0dd3cb33d839ed6f75b6db41e8bbd966e8e3
cip-Mbrain <ennocentcip@gmail.com>
2025-05-23 00:33:01 +0100

    wip

--- challenge.js
+++ challenge.js
@@ -17,5 +17,35 @@ console.log(`The temp is ${getCelsius(100)}\xB0C`);
 /*
 create an arrow function call minMax() that takes in an array of numbers and returns an object with the minimum number in an array
 */
+minMax = (arr) => {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  // console.log(max);
+  return {
+    min: min,
+    max: max,
+  };
+};
 
-const minMax = () => {};
+x = minMax([1, 2, 3, 4, 5, 10]);
+
+// console.log(x);
+
+// create an IFE (immediately invoked function expression) that takes in the length and width of a rectangle outputs it ot the console in a message as soon as the page loads
+
+(function (name) {
+  console.log(`hello ${name}`);
+})('james peters');
+
+(function (lengthOfRectangle, widthOfRectangle) {
+  // return lengthOfRectangle * widthOfRectangle;
+  console.log(lengthOfRectangle * widthOfRectangle);
+})(4, 3);
+
+// program modification.
+((length, width) => {
+  const area = length * width;
+  const output = ` The area if a rectangle with a length of ${length} and a width of ${width} is ${area}`;
+
+  console.log(output);
+})(20, 10);
